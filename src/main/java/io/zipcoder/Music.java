6e376b8967f6c2ad93fcdda26bb9e31024cea7f6
Erick Tepale <e.tepale@gmail.com>
2019-05-31 09:26:20 -0400

    lab finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,54 @@
 package io.zipcoder;
 
+
 public class Music {
 
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+       Boolean flag = true;
+       Integer closestUp = 0;
+       Integer closestDown = 0;
+       Integer counter = 0;
+       Integer index = startIndex;
+
+       //search up
+        while(flag){
+            if(playList[index].equals(selection)){
+                closestUp = counter;
+                break;
+            }
+            if(index == playList.length){
+                index = -1;
+            }
+            counter++;
+            index++;
+        }
+
+        //search down
+        counter = 0;
+        index = startIndex;
+        while(flag){
+            if(playList[index].equals(selection)){
+                closestDown = counter;
+                break;
+            }
+            if(index == 0){
+                index = playList.length;
+            }
+            counter++;
+            index--;
+        }
+
+        //return the biggest one;
+        if(closestDown > closestUp)
+            return closestUp;
+        else
+            return closestDown;
     }
 }
